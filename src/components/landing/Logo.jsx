62094286baa04a740abcd117a77a4b23fd162e0d
Antônio Ferreira <40545248+RABF1969@users.noreply.github.com>
2025-08-10 23:05:26 -0300

    alt no tamanho da logo

--- src/components/landing/Logo.jsx
+++ src/components/landing/Logo.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <img
-        src="/logo.svg"
-        alt="Alfabiz"
-        className="h-14 w-48 object-contain"
-      />
+      <a href="/">
+        <img
+          src="/logo.svg"
+          alt="Alfabiz"
+          className="h-12 w-42 object-contain"
+        />
+      </a>
       <span className="text-2xl font-bold text-foreground"></span>
     </div>
   );
